8a81dc16dba98735235dd3013c8f5b2f38c202f0
unclefomotw <unclefomotw@gmail.com>
2024-04-23 19:16:32 +0800

    Index/Retrieval: Chunks w/ overlap + sentence bound

--- src/rag_7.py
+++ src/rag_7.py
@@ -4,7 +4,7 @@
 # Also, pip install llama-index-vector-stores-qdrant
 
 # This: Use different chunking/indexing strategies
-#       See build_sentence_window_index() / query_sentence_window_index()
+#       Each pair of build_* + query_* invoked by main is one strategy
 #
 # With previous: Vector database as the storage
 #                Embedding model on HuggingFace (pip install llama-index-embeddings-huggingface)
@@ -22,7 +22,10 @@ from llama_index.core import (
     get_response_synthesizer,
 )
 from llama_index.core.indices.vector_store import VectorIndexRetriever
-from llama_index.core.node_parser import SentenceWindowNodeParser
+from llama_index.core.node_parser import (
+    SentenceSplitter,
+    SentenceWindowNodeParser,
+)
 from llama_index.core.postprocessor import MetadataReplacementPostProcessor
 from llama_index.core.query_engine.retriever_query_engine import RetrieverQueryEngine
 from llama_index.embeddings.huggingface import HuggingFaceEmbedding
@@ -81,6 +84,8 @@ def build_sentence_window_index():
     # SentenceWindowNodeParser splits sentences by English punctuation such as ".!?"
     # See split_by_sentence_tokenizer() and llama_index.core.node_parser.text.util
     # To customize, the best bet is to give your own `sentence_splitter` in arg
+
+    # A node is ONE sentence, with a window of sentences as its metadata
     node_parser = SentenceWindowNodeParser.from_defaults(
         window_size=1,  # the real window width is 2*size + 1
         window_metadata_key="window",  # this metadata is excluded in both embed and llm mode
@@ -104,15 +109,12 @@ def build_sentence_window_index():
 
 
 def query_sentence_window_index():
-    # Declare the Qdrant VectorStore; assuming the data is ingested
     db_client = QdrantClient(host="localhost", port=6333)
     vector_store = QdrantVectorStore(
         collection_name="rag_7_sentence_window",
         client=db_client
     )
 
-    # Build via `.from_vector_store` instead of `.from_documents`
-    # The index itself does NOT hold any nodes (see below)
     index = VectorStoreIndex.from_vector_store(vector_store)
 
     retriever = VectorIndexRetriever(
@@ -121,6 +123,7 @@ def query_sentence_window_index():
     )
 
     # Replace node content with the metadata key "window"
+    # While retrieving matches ONE sentence, its window is used for LLM synthesis
     node_postprocessors = [
         MetadataReplacementPostProcessor(target_metadata_key="window")
     ]
@@ -139,9 +142,75 @@ def query_sentence_window_index():
     print(response)
 
 
+def build_chunk_of_sentences_index():
+    # SentenceSplitter splits sentences by English punctuation such as ".!?"
+    # See split_by_sentence_tokenizer() and llama_index.core.node_parser.text.util
+    # To customize, the best bet is to give your own `sentence_splitter` in arg
+
+    # A node is an aggregation of sentences that honer chunk size and overlap.
+    # Or put it another way: a chunk is of its size that honors sentence boundary
+    node_parser = SentenceSplitter(
+        chunk_size=512,
+        chunk_overlap=128
+    )
+
+    nodes = node_parser.get_nodes_from_documents(
+        load_raw_doc()
+    )
+
+    with open("chunk_of_sentences.log", "w") as f:
+        for n in nodes:
+            f.write(f"NODE ID: {n.node_id}\n")
+            if n.prev_node is not None:
+                f.write(f"PREV NODE ID: {n.prev_node.node_id}\n")
+            if n.next_node is not None:
+                f.write(f"NEXT NODE ID: {n.next_node.node_id}\n")
+            f.write("\n>>>content<<<\n")
+            f.write(n.get_content())
+            f.write("\n>>>metadata<<<\n")
+            f.write(n.get_metadata_str())
+            f.write("\n---------\n")
+
+    ingest_to_db(nodes, "rag_7_chunk_of_sentences")
+
+
+def query_chunk_of_sentences_index():
+    db_client = QdrantClient(host="localhost", port=6333)
+    vector_store = QdrantVectorStore(
+        collection_name="rag_7_chunk_of_sentences",
+        client=db_client
+    )
+
+    index = VectorStoreIndex.from_vector_store(vector_store)
+
+    # Simple take the top document(s) from DB, nothing else
+    retriever = VectorIndexRetriever(
+        index=index,
+        similarity_top_k=1,
+    )
+
+    response_synthesizer = get_response_synthesizer(
+        llm=OpenAI(temperature=0.001)
+    )
+
+    query_engine = RetrieverQueryEngine(
+        retriever=retriever,
+        response_synthesizer=response_synthesizer,
+    )
+
+    response = query_engine.query("電腦解魔術方塊的關鍵是什麼？")
+    print(response)
+
+
 if __name__ == '__main__':
     # Sentence Window
     # https://docs.llamaindex.ai/en/stable/examples/node_postprocessor/MetadataReplacementDemo/
 
     # build_sentence_window_index()  # execute this once
-    query_sentence_window_index()  # Can execute separately afterwards
+    # query_sentence_window_index()  # Can execute separately afterwards
+
+    # SentenceSplitter
+    # https://docs.llamaindex.ai/en/stable/module_guides/loading/node_parsers/modules/#sentencesplitter
+
+    # build_chunk_of_sentences_index()  # execute this once
+    query_chunk_of_sentences_index()  # Can execute separately afterwards
